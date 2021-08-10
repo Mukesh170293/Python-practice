7a954464dbcba89e4863282f22abed611d6fb80e
Mukesh170293 <88466840+Mukesh170293@users.noreply.github.com>
2021-08-09 22:03:39 -0700

    Integer to Roman

---
+++ Int_to_rom.py
@@ -0,0 +1,19 @@
+def intToRoman(num):
+     m = [ "", "M", "MM", "MMM" ]
+     c = [ "", "C", "CC", "CCC", "CD", "D",
+           "DC", "DCC", "DCCC", "CM "]
+     x = [ "", "X", "XX", "XXX", "XL", "L",
+           "LX", "LXX", "LXXX", "XC" ]
+     i = [ "", "I", "II", "III", "IV", "V",
+           "VI", "VII", "VIII", "IX"]
+     thousands = m[num // 1000]
+     hundereds = c[(num % 1000) // 100]
+     tens =  x[(num % 100) // 10]
+     ones = i[num % 10]
+
+     ans = (thousands + hundereds +
+                 tens + ones)
+
+     return ans;
+
+print(intToRoman(120))
